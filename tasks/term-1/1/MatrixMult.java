5ad633e13c97615a111cb88e0a295969d5d29d89
Aleximiy <166133923+Aleximiy@users.noreply.github.com>
2025-10-03 21:43:25 +0300

    Update MatrixMult.java
    task 1.1

--- tasks/term-1/1/MatrixMult.java
+++ tasks/term-1/1/MatrixMult.java
@@ -1,6 +1,28 @@
 public class MatrixMult {
-
-	public static double[][] multiply(double[][] firstMatrix, double[][] secondMatrix){
-		return new double[1][1];
+    public static double[][] multiply(double[][] firstMatrix, double[][] secondMatrix){
+        try{
+            if(firstMatrix[0].length != secondMatrix.length) throw new Error();
+            int len1 = firstMatrix.length;
+            int len2 = secondMatrix[0].length;
+            int len3 = firstMatrix[0].length;
+            double[][] res = new double[len1][];
+            
+    		for(int i = 0; i < len1; i++){
+    		    double[] line = new double[len2];
+    		    for(int j = 0; j < len2; j++){
+    		        int sum = 0;
+    		        for(int k = 0; k < len3; k++){
+    		            sum+=firstMatrix[i][k] * secondMatrix[k][j];
+    		        }
+    		        line[j] = sum;
+    		    }
+    		    res[i] = line;
+    		}
+    		
+    		return res;
+        }
+        catch(Exception e){System.err.println("Uncorrect matrixes for multiplication.");}
+        return new double[1][1];
 	}
 }
+
