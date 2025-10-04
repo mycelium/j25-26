08f8ad8e78e301dad82954b6783974b844371438
Marina <marinavasuk2005@mail.ru>
2025-10-04 23:15:26 +0300

    Add task1

--- tasks/term-1/1/MatrixMult.java
+++ tasks/term-1/1/MatrixMult.java
@@ -1,6 +1,77 @@
+
 public class MatrixMult {
 
-	public static double[][] multiply(double[][] firstMatrix, double[][] secondMatrix){
-		return new double[1][1];
-	}
+    //основная функция перемножения матриц
+//    public static double[][] multiply(double[][] firstMatrix, double[][] secondMatrix){
+//        if(firstMatrix[0].length != secondMatrix.length){
+//            System.out.println("Количество столбцов первой матрицы не равно количеству строк второй матрицы, перемножение невозможно");
+//            return null;
+//        }
+//        double[][] result = new double[firstMatrix.length][secondMatrix[0].length];
+//        for(int i =0; i < firstMatrix.length; i++){
+//            for(int j =0; j< secondMatrix[0].length; j++){
+//                result[i][j] =0;
+//                for(int k =0; k < firstMatrix[0].length; k++){
+//                    result[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
+//                }
+//            }
+//        }
+//        return result;
+//    }
+
+    //оптимизированная
+    public static double[][] multiply(double[][] firstMatrix, double[][] secondMatrix){
+        if(firstMatrix[0].length != secondMatrix.length){
+            System.out.println("Количество столбцов первой матрицы не равно количеству строк второй матрицы, перемножение невозможно");
+            return null;
+        }
+        double[][] result = new double[firstMatrix.length][secondMatrix[0].length];
+        for (int i = 0; i < firstMatrix.length; i++) {
+            for (int k = 0; k < firstMatrix[0].length; k++) {
+                double temp = firstMatrix[i][k];
+                for (int j = 0; j < secondMatrix[0].length; j++) {
+                    result[i][j] += temp * secondMatrix[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int s= 500;
+        double[][] A = new double[s][s];
+        double[][] B = new double[s][s];
+
+        for (int i = 0; i < s; i++) {
+            for (int j = 0; j < s; j++) {
+                A[i][j] = Math.random()*10;
+                B[i][j] = Math.random()*10;
+            }
+        }
+
+        //для поиска среднего времени
+        double totalTime = 0;
+        for (int t = 1; t <= 10; t++) {
+            long start = System.nanoTime();
+            double[][] result = multiply(A, B);
+            long end = System.nanoTime();
+
+            double timeMs = (end - start) / 1_000_000.0;
+
+            totalTime += timeMs;
+        }
+
+        double srtime = totalTime / 10;
+        System.out.println(srtime);
+
+        //вывод времени выполнения
+//        long start = System.nanoTime();
+//        double[][] result = multiply(A, B);
+//        long end = System.nanoTime();
+//
+//        double time = (end - start) / 1_000_000.0;
+//        System.out.println(time);
+    }
 }
+
