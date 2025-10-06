0305290c0913ab192c4f71b6497e928f39002b8c
Polina Radionova <halvaahaha@gmail.com>
2025-10-07 00:50:39 +0300

    Task 1: Matrix Multiplication finished

--- tasks/term-1/1/MatrixMult.java
+++ tasks/term-1/1/MatrixMult.java
@@ -1,6 +1,145 @@
 public class MatrixMult {
 
-	public static double[][] multiply(double[][] firstMatrix, double[][] secondMatrix){
-		return new double[1][1];
-	}
+	public static double[][] multiply(double[][] firstMatrix, double[][] secondMatrix) {
+        try {
+            if (firstMatrix == null || secondMatrix == null) {
+                throw new IllegalArgumentsException("Матрицы не могут быть null");
+            }
+
+            if(firstMatrix.length == 0 ||secondMatrix.length == 0) {
+                trhow new IllegalArgumentException("Матрицы не могут быть пустыми");
+            }
+
+            int rowsInA = firstMatrix.length;
+            int rowsInB = secondMatrix.length;
+            int colsInA = firstMatrix[0].length;
+            int colsInB = secondMatrix[0].length;
+
+            if(colsInA != rowsInB){
+                throw new IllegalArgumentException(
+                        String.format("Несовместимые размеры матриц: fristMatrix[%d][%d] и secondMatrix[%d][%d] - количество столбцов первой матрицы должно равняться количеству строк второй матрицы", rowsInA, colsInA, rowsInB, colsInB);
+                )
+            }
+
+            double[][] result = new double[rowsInA][colsInB];
+
+            for(int i = 0;i<rowsInA; i++) {
+                for (int j = 0; j < colsInB; j++) {
+                    double sum = 0.0;
+                    for (int k = 0; k < rowsInB; k++) {
+                        sum += firstMatrix[i][k] * secondMatrix[k][j];
+                    }
+                    result[i][j] = sum;
+                }
+            }
+
+            return result;
+
+        } catch (IllegalArgumentException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ошибка при умножении матриц:" + e.getMessage(), e);
+        }
+    }
+
+    public static double[][] multiplyOptimized(double[][] firstMatrix, double[][] secondMatrix) {
+        try {
+            if (firstMatrix == null || secondMatrix == null) {
+                throw new IllegalArgumentsException("Матрицы не могут быть null");
+            }
+
+            if(firstMatrix.length == 0 ||secondMatrix.length == 0) {
+                trhow new IllegalArgumentException("Матрицы не могут быть пустыми");
+            }
+
+            int rowsInA = firstMatrix.length;
+            int rowsInB = secondMatrix.length;
+            int colsInA = firstMatrix[0].length;
+            int colsInB = secondMatrix[0].length;
+
+            if(colsInA != rowsInB){
+                throw new IllegalArgumentException(
+                        String.format("Несовместимые размеры матриц: fristMatrix[%d][%d] и secondMatrix[%d][%d] - количество столбцов первой матрицы должно равняться количеству строк второй матрицы", rowsInA, colsInA, rowsInB, colsInB);
+                )
+            }
+
+            double[][] result = new double[rowsInA][colsInB];
+            double[][] secondMatrixTransposed = transpose(secondMatrix);
+
+            for(int i = 0;i<rowsInA; i++) {
+                for (int j = 0; j < colsInB; j++) {
+                    double sum = 0.0;
+                    for (int k = 0; k < rowsInB; k++) {
+                        sum += firstMatrix[i][k] * secondMatrix[j][k];
+                    }
+                    result[i][j] = sum;
+                }
+            }
+
+            return result;
+
+        } catch (IllegalArgumentException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ошибка при умножении матриц:" + e.getMessage(), e);
+        }
+    }
+
+    public static double[][] generateMatrix(int rows, int cols) {
+        double[][] matrix = new double[rows][cols];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++) {
+                matrix[i][j] = Math.random();
+            }
+        }
+
+        return matrix;
+    }
+
+    private static double[][] transpose(double[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        double[][] transposed = new double[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+
+        return transposed;
+    }
+
+    public static void main(String[] args) {
+        int size = 2000;
+        System.out.println("Генерация матрицы " + size + "x" + size + "...");
+
+        double[][] A = generateMatrix(size, size);
+        double[][] B = generateMatrix(size, size);
+
+        int numOfExperiments = 10;
+        long total = 0;
+
+        System.out.println("Стандартный подход");
+        for (int exp = 0; exp < numOfExperiments; exp++) {
+            long start = System.currentTimeMillis();
+            multiply(A, B);
+            long end = System.currentTimeMillis();
+            long duration = end - start;
+            total += duration;
+
+            System.out.println("Время выполнения для итерации " + (exp + 1) + " составило " + duration + " мс");
+        }
+
+        System.out.println("Оптимизированный подход");
+        for (int exp = 0; exp < numOfExperiments; exp++) {
+            long start = System.currentTimeMillis();
+            multiplyOptimized(A, B);
+            long end = System.currentTimeMillis();
+            long duration = end - start;
+            total += duration;
+
+            System.out.println("Время выполнения для итерации " + (exp + 1) + " составило " + duration + " мс");
+        }
+    }
 }
