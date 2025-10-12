b86e06198341a98f1bbb81d4dd4cf5e5cc85eefc
Vladimir Shishmakov <vova2005761@gmail.com>
2025-10-12 22:41:56 +0300

    second java task done

--- tasks/term-1/4/WordFrequencyCounter.java
+++ tasks/term-1/4/WordFrequencyCounter.java
@@ -1,31 +1,41 @@
 import java.util.*;
 import java.nio.file.Path;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 
 public class WordFrequencyCounter {
-    private static final long changeModeSize = 2 * 1024 * 1024;
-    private static final int blockSize = 1024 * 1024;
+    private static final long changeModeSize = 512 * 1024;
+    private static final int blockSize = 512 * 1024;
 
     private Map<String, Integer> countWordsSmall (Path filePath) {
-        Map<String, Integer> frequencyMap = new hashMap<>();
+        Map<String, Integer> frequencyMap = new HashMap<>();
         
         try {
             String fileString = Files.readString(filePath);
-            StringBuilder currentWord;
+            StringBuilder currentWord = new StringBuilder();
             
-            for (int i = 0; i < fileString.length; i++) {
+            for (int i = 0; i < fileString.length(); i++) {
                 char symbol = fileString.charAt(i);
 
                 if (Character.isLetterOrDigit(symbol)) {
                     currentWord.append(symbol);
                 }
                 else {
-                    String newWord = currentWord.toString();
-                    frequencyMap.put(newWord, frequencyMap.getOrDefault(newWord, 0) + 1);
-                    currentWord.setLength(0);
+                    if (currentWord.length() > 0){
+                        String newWord = currentWord.toString().toLowerCase();
+                        frequencyMap.put(newWord, frequencyMap.getOrDefault(newWord, 0) + 1);
+                        currentWord.setLength(0);
+                    }
                 }
             }
+
+            if (currentWord.length() > 0) {
+                String newWord = currentWord.toString().toLowerCase();
+                frequencyMap.put(newWord, frequencyMap.getOrDefault(newWord, 0) + 1);
+                currentWord.setLength(0);
+            }
+
         }
         catch (IOException e) {
             System.err.println("Something went wrong: " + e.getMessage());
@@ -34,16 +44,85 @@ public class WordFrequencyCounter {
         return frequencyMap;
     }
 
+    private Map<String, Integer> countWordsBig (Path filePath) {
+        Map <String, Integer> frequencyMap = new HashMap<>();
+        StringBuilder incompleteWord = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(Files.newBufferedReader(filePath), blockSize)) {
+            char[] buffer = new char[blockSize];
+            int symbolCode;
+
+            while ((symbolCode = reader.read(buffer)) != -1) {    
+                String currentBlock = incompleteWord.toString() + new String(buffer, 0, symbolCode);    
+                incompleteWord.setLength(0);
+
+                StringBuilder currentWord = new StringBuilder();
+
+                for (int i = 0; i < currentBlock.length(); i++) {
+                    char c = currentBlock.charAt(i);
+                    if (Character.isLetterOrDigit(c)) {
+                        currentWord.append(c);
+                    }
+                    else {
+                        if (currentWord.length() > 0) {
+                            String newWord = currentWord.toString().toLowerCase();
+                            frequencyMap.put(newWord, frequencyMap.getOrDefault(newWord, 0) + 1);
+                            currentWord.setLength(0);
+                        }
+                    }
+                }
+
+                if (currentWord.length() > 0) {
+                    incompleteWord.append(currentWord);
+                }
+                
+            }
+
+            if (incompleteWord.length() > 0) {
+                String newWord = incompleteWord.toString().toLowerCase();
+                frequencyMap.put(newWord, frequencyMap.getOrDefault(newWord, 0) + 1);
+            }
+
+        } 
+        catch (IOException e) {
+            System.err.println("Something went wrong: " + e.getMessage());
+        }
+
+
+        return frequencyMap;
+    }
+
     public Map<String, Integer> countWords(Path filePath) {
-        // read file, tokenize words, update map
-        return null;
+        try {
+        if (Files.size(filePath) > changeModeSize) {
+            return countWordsBig(filePath);
+        }
+        else {
+            return countWordsSmall(filePath);
+        }
+        }
+        catch (IOException e) {
+            System.err.println("Something went wrong: " + e.getMessage());
+            return Collections.emptyMap();
+        }
     }
 
     public void printFrequencies(Map<String, Integer> frequencies) {
-        // print word counts
+        System.out.println("------------- Word Frequencies -------------");
+        frequencies.forEach((word, count) -> System.out.println("[" + word + " : " + count + "]"));
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        // run word frequency counter
+        WordFrequencyCounter counter = new WordFrequencyCounter();
+        Map<String, Integer> smallFileCounter = counter.countWords(Path.of("term-1/4/smallFile.txt"));
+
+        counter.printFrequencies(smallFileCounter);
+        
+        Map<String, Integer> bigFileCounter = counter.countWords(Path.of("term-1/4/bigFile.txt"));
+
+        counter.printFrequencies(bigFileCounter);
+        
+
     }
 }
