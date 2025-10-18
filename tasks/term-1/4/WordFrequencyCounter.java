507dcbd0aeb17c377cc2e76c8f50e1bef08ab437
perajush <perajush@gmail.com>
2025-10-18 12:05:52 +0300

    a

--- tasks/term-1/4/WordFrequencyCounter.java
+++ tasks/term-1/4/WordFrequencyCounter.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.lang.management.GarbageCollectorMXBean;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
