8d867ddeb85f0c70edbbc13c8ef9e36b792c1643
Vladimir Shishmakov <vova2005761@gmail.com>
2025-10-19 19:33:57 +0300

    counting change done

--- tasks/term-1/2/Main.scala
+++ tasks/term-1/2/Main.scala
@@ -43,7 +43,10 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) 1
+    else if (money < 0) 0
+    else if (coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
   
   /**
