64ae12f6b77d872697f13298b7c1d0eaf9afffc7
feroxies <annelaishevkina@gmail.com>
2025-10-20 18:00:52 +0300

    Сделана 3 лабораторная

--- tasks/term-1/3/src/main/scala/funsets/Main.scala
+++ tasks/term-1/3/src/main/scala/funsets/Main.scala
@@ -2,5 +2,44 @@ package funsets
 
 object Main extends App {
   import FunSets._
-  println(contains(singletonSet(1), 1))
+
+  val s1 = singletonSet(1)
+  val s2 = singletonSet(2)
+  val s3 = singletonSet(3)
+  println(contains(s1, 1))
+  println(contains(s2, 1))
+
+  println("Union: ")
+  val usa = union(s1, s2)
+  printSet(usa)
+  val usb = union(s1, s3)
+  printSet(usb)
+
+  println("Intersection: ")
+  val is = intersect(usa, usb)
+  printSet(is)
+
+  println("Diff: ")
+  val ds = diff(usa, usb)
+  printSet(ds)
+
+  println("Filter: ")
+  val s4 = singletonSet(4)
+  val s1234 = union(usa, union(s3, s4))
+  val even = filter(s1234, x => x % 2 == 0)
+  printSet(even)
+
+  println("Forall: ")
+  println(forall(s1234, x => x > 0))
+  println(forall(s1234, x => x < 2))
+
+  println("Exists: ")
+  println(exists(s1234, x => x < 0))
+  println(exists(s1234, x => x > 2))
+
+  println("Map: ")
+  val squares = map(s1234, x => x * x)
+  printSet(squares)
+
+
 }
