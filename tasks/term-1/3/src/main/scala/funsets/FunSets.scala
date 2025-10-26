c4fae08f13efdd83aedbfc3dce750a1c9269796b
Egorov SA <egorovsv2005@yandex.ru>
2025-10-26 16:30:36 +0300

    Task 3 is done

--- tasks/term-1/3/src/main/scala/funsets/FunSets.scala
+++ tasks/term-1/3/src/main/scala/funsets/FunSets.scala
@@ -1,6 +1,6 @@
 package funsets
 
-import common._
+//import common._
 
 /**
  * 2. Purely Functional Sets.
@@ -20,31 +20,31 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
+    def singletonSet(elem: Int): Set = (arg: Int) => arg == elem
   
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
+    def union(s: Set, t: Set): Set = (arg: Int) => s(arg) || t(arg)
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+    def intersect(s: Set, t: Set): Set = (arg: Int) => s(arg) && t(arg)
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set = (arg: Int) => s(arg) && !t(arg)
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+    def filter(s: Set, p: Int => Boolean): Set = (arg: Int) => s(arg) && p(arg)
   
 
   /**
@@ -56,24 +56,24 @@ object FunSets {
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
     def forall(s: Set, p: Int => Boolean): Boolean = {
-    def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
-    }
-    iter(???)
+      def iter(a: Int): Boolean = {
+        if (a > bound) true
+        else if (contains(s, a) && !p(a)) false 
+        else iter(a + 1)
+      }
+    iter(-bound)
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+    def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, arg => !p(arg))
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+    def map(s: Set, f: Int => Int): Set = (arg2: Int) => exists(s, arg1 => f(arg1) == arg2)
   
   /**
    * Displays the contents of a set
@@ -86,7 +86,7 @@ object FunSets {
   /**
    * Prints the contents of a set on the console.
    */
-  def printSet(s: Set) {
+  def printSet(s: Set): Unit = {
     println(toString(s))
   }
 }
--- tasks/term-1/3/src/main/scala/funsets/Main.scala
+++ tasks/term-1/3/src/main/scala/funsets/Main.scala
@@ -2,5 +2,45 @@ package funsets
 
 object Main extends App {
   import FunSets._
-  println(contains(singletonSet(1), 1))
+  println("Test FunSets.scala:")
+  val s1 = singletonSet(1)
+  val s2 = singletonSet(2)
+
+  println("--singletonSet: --")
+  println("contains(singletonSet(1), 1) = " + contains(singletonSet(1), 1))
+  println("contains({1}, 1) = " + contains(s1, 1))
+  println("contains({1}, 2) = " + contains(s1, 2))
+  println("contains({2}, 2) = " + contains(s2, 2))
+
+  println("--union: --")
+  val s12 = union(s1, s2)
+  println("union({1}, {2}) contains 1 = " + contains(s12, 1))
+  println("union({1}, {2}) contains 2 = " + contains(s12, 2))
+  println("union({1}, {2}) contains 3 = " + contains(s12, 3))
+
+  println("--intersect: --")
+  val inter = intersect(s12, s2)
+  println("intersect({1,2}, {2}) contains 1) = " + contains(inter, 1))
+  println("intersect({1,2}, {2}) contains 2) = " + contains(inter, 2))
+
+  println("--diff: --")
+  val sDiff = diff(s12, s1)
+  println("diff({1,2}, {1}) contains 1) = " + contains(sDiff, 1))
+  println("diff({1,2}, {1}) contains 2) = " + contains(sDiff, 2))
+
+  println("--filter: --")
+  val sFilt = filter(s12, x => x % 2 == 0)
+  println("filter({1,2}, x%2==0) contains 1) = " + contains(sFilt, 1))
+  println("filter({1,2}, x%2==0) contains 2) = " + contains(sFilt, 2))
+
+  println("--map: --")
+  val sMap = map(s12, x => x * 10)
+  println("map({1,2}, x*10) contains 10) = " + contains(sMap, 10))
+  println("map({1,2}, x*10) contains 20) = " + contains(sMap, 20))
+  println("map({1,2}, x*10) contains 2) = " + contains(sMap, 2))
+
+  println("--printSet: --")
+  val sPrint = union(union(singletonSet(15), singletonSet(20)), singletonSet(30))
+  println("sPrint: ")
+  printSet(sPrint)
 }
