06b6dfd9d3e6aef1f06ae6b7967273edbe2e46e8
VeronikaLaz <iazareva-v-i@mail.ru>
2025-10-29 02:14:07 +0300

    2 task completed

--- tasks/term-1/2/Main.scala
+++ tasks/term-1/2/Main.scala
@@ -11,18 +11,43 @@ object Main {
     }
   }
 
+    println("\nTesting balance function:")
+    println(balance("(just (an) example)".toList)) 
+    println(balance("())(".toList)) 
+    
+    println("\nTesting countChange:")
+    println(countChange(4, List(1,2)))
+    
+    println("\nTesting nQueens:")
+    val solution = nQueens(4)
+    solution match {
+      case Some(board) => printBoard(board)
+      case None => println("No solution found")
+    }
+  }
+
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def balanceHelper(chars: List[Char], openCount: Int): Boolean = {
+      if (openCount < 0) false
+      else chars match {
+        case Nil => openCount == 0
+        case '(' :: tail => balanceHelper(tail, openCount + 1)
+        case ')' :: tail => balanceHelper(tail, openCount - 1)
+        case _ :: tail => balanceHelper(tail, openCount)
+      }
+    }
+    balanceHelper(chars, 0)
   }
 
   /**
@@ -33,7 +58,11 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else {
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
   }
   
   /**
@@ -49,7 +78,44 @@ object Main {
    */
 
   def nQueens(size: Int): Option[Array[Int]] = {
+    def isSafe(queens: Array[Int], row: Int, col: Int): Boolean = {
+      for (i <- 0 until row) {
+        if (queens(i) == col || math.abs(queens(i) - col) == math.abs(i - row)) {
+          return false
+        }
+      }
+      true
+    }
+    
+    def placeQueens(queens: Array[Int], row: Int): Boolean = {
+      if (row == size) true
+      else {
+        for (col <- 0 until size) {
+          if (isSafe(queens, row, col)) {
+            queens(row) = col
+            if (placeQueens(queens, row + 1)) {
+              return true
+            }
+          }
+        }
+        false 
+      }
+    }
 
+    val queens = Array.fill(size)(-1)
+    if (placeQueens(queens, 0)) Some(queens)
+    else None
+  }
+
+  def printBoard(queens: Array[Int]): Unit = {
+    val size = queens.length
+    for (i <- 0 until size) {
+      for (j <- 0 until size) {
+        if (queens(i) == j) print("1 ")
+        else print("0 ")
+      }
+      println()
+    }
   }
 
 }
