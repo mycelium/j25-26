6bf2c9f788f5003ea72181b349cd910d40c5bb7d
Ratmir <ratmairiss@mail.ru>
2025-10-29 22:49:37 +0800

    uploaded Task 2 code

--- tasks/term-1/2/Main.scala
+++ tasks/term-1/2/Main.scala
@@ -1,55 +1,149 @@
-package recfun
-import common._
-
 object Main {
-  def main(args: Array[String]) {
+
+  def main(args: Array[String]): Unit = {
+
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
+    println()
+
+    println("Parentheses Balancing")
+    for(str <- Array("(if (zero? x) max (/ 1 x))",
+                     "I told him (that it's not (yet) done). (But he wasn't listening)",
+                     ":-)", "())(", "", "()", "((()))", "(()))(", "((()", "hello world",
+                     "(a (b (c)) d)", "(a (b (c)) d))", "((a (b (c)) d)", ")(", "())", "((())",
+                     "(word (nested (deep)) text)", "(word (nested (deep) text)",
+                     "(word (nested deep)) text)", "(()()()())", "(((())))", "(()((())()))",
+                     "(()((())())")){
+      println(s"\"$str\" is ${if (balance(str.toList)) "" else "not" } balanced")
+    }
+    println()
+
+    println("Counting Change")
+    for(m <- 0 to 5) {
+      for(coinAmt <- 1 to 4) {
+        var lst = (1 to coinAmt).toList
+        print(s"There is(are) ${countChange(m,lst)} way(s) to pay $m with")
+        lst.foreach(el => print(s" $el"))
+        println()
+      }
+    }
+    println()
+
+    println("N-Queens Problem")
+    for(size <- 1 to 10) {
+      println(s"Size = $size")
+      var opBoard = nQueens(size)
+      if (opBoard != None) printBoard(opBoard.get)
+      else println("No solution")
+      println()
+    }
+
+
   }
 
-  /**
-   * Exercise 1
-   */
   def pascal(c: Int, r: Int): Int = {
 
+    if(c < 0 || r < 0)
+      throw new IllegalArgumentException(s"Column and Row number can't be negative.")
+
+    def goingUp(c: Int, r: Int): Int = {
+      if (c != 0 && c != r)
+        goingUp(c - 1, r - 1) + goingUp(c, r - 1)
+      else 1
+    }
+
+    goingUp(c,r)
   }
 
-  /**
-   * Exercise 2 Parentheses Balancing
-   */
   def balance(chars: List[Char]): Boolean = {
-   
+
+    def checkNextChar(charsLeft: List[Char], unclosedAmt: Int): Boolean = {
+      // unclosedAmt - Количество незакрытых ( в рассмотренной строке
+
+      if (charsLeft.isEmpty) unclosedAmt == 0
+      else{
+        charsLeft.head match {
+          case '(' => checkNextChar(charsLeft.tail, unclosedAmt + 1)
+          case ')' => { if (unclosedAmt == 0) false
+                        else checkNextChar(charsLeft.tail, unclosedAmt - 1) }
+          case _ => checkNextChar(charsLeft.tail, unclosedAmt)
+        }
+      }
+    }
+    checkNextChar(chars, 0)
   }
 
-  /**
-   * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
-   */
   def countChange(money: Int, coins: List[Int]): Int = {
 
+    if (money < 0)
+      throw new IllegalArgumentException("Amount of money cannot be negative")
+
+    def countChange0(moneyLeft: Int, coinsLeft: List[Int]): Int = {
+      if (moneyLeft == 0) 1
+      else if (coinsLeft.isEmpty || moneyLeft < 0) 0
+      else countChange0(moneyLeft, coinsLeft.tail) +
+        countChange0(moneyLeft - coinsLeft.head, coinsLeft)
+    }
+    countChange0(money, coins)
   }
-  
-  /**
-   * Excerice 4 N-Queens Problem
-   * Write a function that provides a solution for n-queens problem if possible
-   * Input parameter represents board size and number of queens
-   * Output is an array: index represents row number and value represents column number.
-   * Example: nQueens(4): [1,3,0,2] or
-   * 0 1 0 0
-   * 0 0 0 1
-   * 1 0 0 0
-   * 0 0 1 0
-   */
 
   def nQueens(size: Int): Option[Array[Int]] = {
 
+    if (size < 0)
+      throw new IllegalArgumentException("Size can't be negative")
+
+    def changePos(qToChange: Int, pos: Int, board: Array[Int]): Option[Array[Int]] = {
+      // qToChange - Какую фигуру(строку) меняем;
+      // pos - На какой позиции находится фигура сейчас;
+
+      // Дошли до конца строки => С такой постановкой предыдущих
+      // королев нет вариантов как поставить королеву на новую
+      // строку => Возвращаемся на строку вверх и переставляем фигуру
+      if (pos == board.size + 1) {
+        if (qToChange == 1) // Если первая фигура дошла
+          return None       // до конца строки => Нет решения
+        changePos(qToChange - 1, board(qToChange - 2) + 1, board)
+      } else if (qToChange == board.size + 1) // Успешно Дошли до последней фигуры
+        Some(board)
+      else {                                  // Проверка на корректность постановки
+        if (isSafe(qToChange, pos, board)) {
+          board(qToChange - 1) = pos
+          changePos(qToChange + 1, 1, board)
+        } else {
+          // Есть мешающая королева => Идем на следующий столбец
+          changePos(qToChange, pos + 1, board)
+        }
+      }
+    }
+
+    def isSafe(row: Int, col: Int, board: Array[Int]): Boolean = {
+      var res = true
+      for (i <- 1 until row) {
+        val higherCol = board(i - 1)
+        if (higherCol == col ||           // По вертикали
+          higherCol == col + (i - row) || // По диагонали влево
+          higherCol == col - (i - row))   // По диагонали вправо
+          res = false
+      }
+      res
+    }
+
+    val tmp = Array.fill(size)(0)
+    changePos(1,1,tmp)
+  }
+
+  def printBoard(board: Array[Int]): Unit ={
+    for(i <- 0 until board.size) {
+      for(j <- 0 until board.size){
+        print((if (board(i) == j + 1) 1 else 0) + " ")
+      }
+      println()
+    }
   }
 
 }
+
