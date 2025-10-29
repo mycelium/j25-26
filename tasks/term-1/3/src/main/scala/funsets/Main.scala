341df4b936b75393c1a2884fc35aab565194509e
mindwq <rsasa268@gmail.com>
2025-10-29 19:20:39 +0300

    task 3 is completed

--- tasks/term-1/3/src/main/scala/funsets/Main.scala
+++ tasks/term-1/3/src/main/scala/funsets/Main.scala
@@ -2,5 +2,19 @@ package funsets
 
 object Main extends App {
   import FunSets._
-  println(contains(singletonSet(1), 1))
+
+  val s = singletonSet(1)
+  val t = singletonSet(2)
+  val u = union(s, t)         
+
+  println("u = " + toString(u))               
+
+  println("forall(u, _ < 3) = " + forall(u, _ < 3))  
+  println("forall(u, _ % 2 == 0) = " + forall(u, _ % 2 == 0))
+
+  println("exists(u, _ == 2) = " + exists(u, _ == 2)) 
+  println("exists(u, _ == 3) = " + exists(u, _ == 3)) 
+
+  val m = map(u, _ * 2)        
+  println("map(u, _ * 2) = " + toString(m))
 }
