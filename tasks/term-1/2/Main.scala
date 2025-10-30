721e33aa1f94dad37b314486c5a09212ce00f651
Med <medsalemabbad1@gmail.com>
2025-10-30 17:34:26 +0300

    lab 2/3

--- tasks/term-1/2/Main.scala
+++ tasks/term-1/2/Main.scala
@@ -1,5 +1,4 @@
 package recfun
-import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -9,47 +8,94 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("\nBalance tests:")
+    println(balance("(if (zero? x) max (/ 1 x))".toList))
+    println(balance("I told him (that it's not (yet) done). (But he wasn't listening)".toList))
+    println(balance(":-)".toList))
+    println(balance("())(".toList))
+
+    println("\nCount Change tests:")
+    println(s"countChange(4, List(1,2)): ${countChange(4, List(1,2))}")
+    println(s"countChange(5, List(2,3)): ${countChange(5, List(2,3))}")
+    println(s"countChange(10, List(1,5,10)): ${countChange(10, List(1,5,10))}")
+    println(s"countChange(0, List(1,2)): ${countChange(0, List(1,2))}")
+    println(s"countChange(5, List()): ${countChange(5, List())}")
+    println(s"countChange(-5, List(1,2)): ${countChange(-5, List(1,2))}")
+
+    println("\nN-Queens for size 4:")
+    nQueens(4) match {
+      case Some(solution) => printBoard(solution)
+      case None => println("No solution")
+    }
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if(c < 0 || r < 0 || c > r) 0
+    else if(c == 0 || c == r) 1
+    else pascal(c-1,r-1) + pascal(c,r-1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def check(cs: List[Char], open: Int): Boolean = {
+      if(open < 0) false
+      else if(cs.isEmpty) open == 0
+      else if(cs.head == '(') check(cs.tail, open + 1)
+      else if(cs.head == ')') check(cs.tail, open - 1)
+      else check(cs.tail, open)
+    }
+    check(chars, 0)
   }
 
   /**
    * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else if (coins.head <= 0) countChange(money, coins.tail)
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
-  
+
   /**
-   * Excerice 4 N-Queens Problem
-   * Write a function that provides a solution for n-queens problem if possible
-   * Input parameter represents board size and number of queens
-   * Output is an array: index represents row number and value represents column number.
-   * Example: nQueens(4): [1,3,0,2] or
-   * 0 1 0 0
-   * 0 0 0 1
-   * 1 0 0 0
-   * 0 0 1 0
+   * Exercise 4 N-Queens Problem
    */
-
   def nQueens(size: Int): Option[Array[Int]] = {
+    if(size <= 0) return None
 
+    def isSafe(board: List[Int], row: Int, col: Int): Boolean = {
+      board.zipWithIndex.forall { case (c, r) =>
+        c != col && math.abs(c - col) != math.abs(r - row)
+      }
+    }
+
+    def solve(row: Int, board: List[Int]): Option[List[Int]] = {
+      if(row == size) Some(board)
+      else {
+        (0 until size).flatMap { col =>
+          if(isSafe(board, row, col)) solve(row + 1, board :+ col)
+          else None
+        }.headOption
+      }
+    }
+
+    solve(0, List()).map(_.toArray)
   }
 
+  def printBoard(solution: Array[Int]): Unit = {
+    println("Solution: " + solution.mkString("[", ",", "]"))
+    for(row <- solution.indices) {
+      for(col <- solution.indices) {
+        if(solution(row) == col) print("1 ")
+        else print("0 ")
+      }
+      println()
+    }
+  }
 }
--- tasks/term-1/3/src/main/scala/funsets/Main.scala
+++ tasks/term-1/3/src/main/scala/funsets/Main.scala
@@ -1,6 +1,50 @@
-package funsets
+//package funsets
 
 object Main extends App {
   import FunSets._
-  println(contains(singletonSet(1), 1))
+
+  // Создаем одноэлементные множества
+  val s1 = singletonSet(1)
+  val s2 = singletonSet(2)
+  val s3 = singletonSet(3)
+
+  println("s1 содержит 1: " + contains(s1, 1))  // true
+  println("s1 содержит 2: " + contains(s1, 2))  // false
+
+  // Объединение множеств
+  val s123 = union(union(s1, s2), s3)
+  println("Объединение {1}, {2}, {3}:")
+  printSet(s123)  // {1,2,3}
+
+  // Пересечение множеств
+  val s12 = union(s1, s2)
+  val s23 = union(s2, s3)
+  val intersection = intersect(s12, s23)
+  println("Пересечение {1,2} и {2,3}:")
+  printSet(intersection)  // {2}
+
+  // Разность множеств
+  val difference = diff(s123, s2)
+  println("Разность {1,2,3} и {2}:")
+  printSet(difference)  // {1,3}
+
+  // Фильтрация
+  val evens = filter(s123, (x: Int) => x % 2 == 0)
+  println("Четные числа из {1,2,3}:")
+  printSet(evens)  // {2}
+
+  // Множество всех четных чисел (в пределах bound)
+  val allEvens: Set = (x: Int) => x % 2 == 0
+  println("Все четные числа существуют: " +
+    exists(allEvens, (x: Int) => x > 0))  // true
+
+  // Проверка для всех элементов
+  println("Все элементы {2,4,6} четные: " +
+    forall(union(union(singletonSet(2), singletonSet(4)), singletonSet(6)),
+      (x: Int) => x % 2 == 0))  // true
+
+  // Отображение множества
+  val doubled = map(s123, (x: Int) => x * 2)
+  println("Удвоенные элементы {1,2,3}:")
+  printSet(doubled)  // {2,4,6}
 }
