8cff40c77418ca08f060043a98464f32d0b67051
Maxim Fomin <fominm939@gmail.com>
2025-10-30 20:44:50 +0300

    Add scala lab_02, lab_03

--- tasks/term-1/2/Main.scala
+++ tasks/term-1/2/Main.scala
@@ -1,28 +1,55 @@
 package recfun
-import common._
 
 object Main {
-  def main(args: Array[String]) {
+  def main(args: Array[String]) = {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
+    println("Тестирование Exercise 2 Parentheses Balancing:")
+    println("(2+5) * ((4 * 10)) : " + balance("(2+5) * ((4 * 10))".toList))
+    println("2+5) * (4 * 10)) : " + balance("2+5) * (4 * 10))".toList))
+    println("Тестирование Exercise 3 Counting Change:")
+    println("2, List(1,2): " + countChange(2, List(1, 2)))
+    println("7, List(1,2,3): " + countChange(7, List(1, 2, 3)))
+    println("Тестирование Excerice 4 N-Queens Problem:")
+    nQueens(5) match {
+      case Some(finding) => printBoard(finding)
+      case None =>
+    }
+
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def auxiliaryDef(chars: List[Char], count: Int): Boolean = {
+      if (chars.isEmpty) {
+        count == 0
+      }
+      else if (count < 0) {
+        false
+      }
+      else chars.head match {
+        case '(' => auxiliaryDef(chars.tail, count + 1)
+        case ')' => auxiliaryDef(chars.tail, count - 1)
+        case _ => auxiliaryDef(chars.tail, count)
+
+      }
+    }
+
+    auxiliaryDef(chars, 0)
   }
 
   /**
@@ -33,9 +60,17 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) {
+      1
+    }
+    else if (money < 0 || coins.isEmpty) {
+      0
+    }
+    else {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
   }
-  
+
   /**
    * Excerice 4 N-Queens Problem
    * Write a function that provides a solution for n-queens problem if possible
@@ -49,7 +84,46 @@ object Main {
    */
 
   def nQueens(size: Int): Option[Array[Int]] = {
+    def notEaten(positionQ: Array[Int], rowNewQ: Int, colNewQ: Int): Boolean = {
+      for (i <- 0 until rowNewQ) {
+        if (positionQ(i) == colNewQ || math.abs(positionQ(i) - colNewQ) == math.abs(i - rowNewQ)) {
+          return false
+        }
 
+      }
+      true
+    }
+
+    def finding(row: Int, positionQ: Array[Int]): Boolean = {
+      if (row == size) {
+        true
+      }
+      else {
+        (0 until size).exists { y =>
+          notEaten(positionQ, row, y) && {
+            positionQ(row) = y
+            finding(row + 1, positionQ)
+          }
+        }
+      }
+    }
+    val positionQ = new Array[Int](size)
+    val findVal = finding(0, positionQ)
+    if (findVal) Some(positionQ) else None
+
+  }
+
+  def printBoard(positionQ: Array[Int]): Unit = {
+    val size = positionQ.length
+
+    for(x <- 0 until size)
+    {
+      for (y <- 0 until size)
+      {
+        if (positionQ(x) == y) print("1 ") else print("0 ")
+      }
+      println()
+    }
   }
 
 }
--- tasks/term-1/3/src/main/scala/funsets/Main.scala
+++ tasks/term-1/3/src/main/scala/funsets/Main.scala
@@ -3,4 +3,49 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+
+  val s03 = singletonSet(3)
+  val s04 = singletonSet(4)
+  println("Тесты:")
+  println("union:")
+  val un01 = union(s03, s04)
+  println("Объединение 3 и 4 проверка 2: " + contains(un01, 2));
+  println("Объединение 3 и 4 проверка 3: " + contains(un01, 3));
+  println("Объединение 3 и 4 проверка 4: " + contains(un01, 4));
+
+  println("intersect:")
+  val per01 = intersect(un01, s03)
+  println("Пересечение {3,4} c {3} проверка 4: " + contains(per01, 4));
+  println("Пересечение {3,4} c {3} проверка 3: " + contains(per01, 3));
+
+  val paz01 = diff(un01, s03)
+  println("diff:")
+  println("Разность {3,4} c {3} проверка 4: " + contains(paz01, 4));
+  println("Разность {3,4} c {3} проверка 3: " + contains(paz01, 3));
+
+  println("filter:")
+
+  val per1234 = union(union(singletonSet(1), singletonSet(2)), union(singletonSet(3), singletonSet(4)))
+  val fil01 = filter(per1234, _ < 2)
+  println("(Чисела < 2 из {1,2,3,4}) проверка 1: " + contains(fil01, 1))
+  println("(Чисела < 2 из {1,2,3,4}) проверка 2: " + contains(fil01, 2))
+
+  val sset = union(singletonSet(-1), per1234)
+  val foeall01 = forall(sset, _ > 0)
+  println("Все числа в {-1,1,2,3,4} положительные: " + foeall01)
+
+  println("exists:")
+  val ectOtr = exists(sset, _ < 0)
+  println("В {-1,1,2,3,4} есть отрицательные: " + ectOtr)
+
+  println("map:")
+  val doubl = map(per1234, _ * 2)
+  printSet(per1234)
+  println("Удвоенные числа из {1,2,3,4} проверка 2: " + contains(doubl, 2))
+
+  printSet(doubl)
+
+
+
+
 }
