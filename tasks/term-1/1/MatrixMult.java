19cddc7964b0939c285eefc69bc175d86a5bcbc7
Aleximiy <166133923+Aleximiy@users.noreply.github.com>
2025-10-30 18:53:28 +0300

    Update MatrixMult.java

--- tasks/term-1/1/MatrixMult.java
+++ tasks/term-1/1/MatrixMult.java
@@ -26,5 +26,34 @@ public class MatrixMult {
         catch(Exception e){System.err.println("Uncorrect matrixes for multiplication.");}
         return new double[1][1];
 	}
+
+	public static void main(String[] args) {
+    	double[][] A = {{1, 2}, {3, 2}};
+        double[][] B = {{3, 1}, {4, 4}};
+        double[][] C = {{11, 9}, {17, 11}};
+        double[][] res = multiply(A, B);
+    
+        boolean ok = true;
+    	for (int i = 0; i < C.length; i++) {
+    		for (int j = 0; j < C[0].length; j++) {
+    			if (res[i][j] - C[i][j] != 0) {
+    				ok = false;
+    			}
+    		}
+    	}
+        System.out.print("test 1: ");
+        System.out.println(ok);
+
+        int size = 20;
+        double[][] A2 = new double[size][size];
+        double[][] B2 = new double[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                A2[i][j] = Math.random();
+                B2[i][j] = Math.random();
+            }
+        }
+        double[][] res2 = MatrixMult.multiply(A2, B2);
+    }
 }
 
