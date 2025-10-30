fa00a656bef6ed4fa6d1d4638466bca4258cfa0d
Alena Mikhaylova <156425549+truknok@users.noreply.github.com>
2025-10-30 15:20:04 +0300

    Update Main.scala

--- tasks/term-1/3/src/main/scala/funsets/Main.scala
+++ tasks/term-1/3/src/main/scala/funsets/Main.scala
@@ -2,5 +2,35 @@ package funsets
 
 object Main extends App {
   import FunSets._
-  println(contains(singletonSet(1), 1))
+  
+  val s1 = singletonSet(1)
+  val s2 = singletonSet(2)
+  println(contains(s1, 3))
+  println(contains(s2, 2))
+  
+  val u = union(s1, s2)
+  printSet(u)
+  
+  val i = intersect(u, s1)
+  printSet(i)
+  
+  val d = diff(u, singletonSet(2))
+  printSet(d)
+
+  val s_base = union(singletonSet(5), singletonSet(10))
+  val is_gt_7: Int => Boolean = x => x > 7
+  val result = filter(s_base, is_gt_7) 
+  printSet(result)
+  
+  val ot = union(s1, s2) 
+  val positive: Int => Boolean = x => x > 0 
+  val pgt1: Int => Boolean = x => x > 1 
+  println(forall(ot, positive))
+  println(forall(ot, pgt1))
+
+  println(exists(ot, (x: Int) => x > 1))
+  
+  val f_plus_10: Int => Int = x => x + 10
+  printSet(ot)
+  printSet(map(ot, f_plus_10))
 }
