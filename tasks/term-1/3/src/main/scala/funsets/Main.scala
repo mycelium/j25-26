4530833b183a9da8b4cb074955db4eeac33b6c6e
daniilkonovalov <danechkamsmk@gmail.com>
2025-11-01 22:44:17 +0300

    выполнил lab3

--- tasks/term-1/3/src/main/scala/funsets/Main.scala
+++ tasks/term-1/3/src/main/scala/funsets/Main.scala
@@ -3,4 +3,15 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+
+  val s1 = singletonSet(1)
+  val s2 = singletonSet(2)
+  val u = union(s1, s2)
+
+  println(contains(u, 1))
+  println(contains(u, 3))
+
+  val mapped = map(u, _ * 2)
+  println(contains(mapped, 2))
+  println(contains(mapped, 4))
 }
