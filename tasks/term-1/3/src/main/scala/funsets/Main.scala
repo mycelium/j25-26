984e5cbc04c93e41f5a9f0bb03b9171c107391e7
kozlovskaya-oa <94537045+kozlovskaya-oa@users.noreply.github.com>
2025-11-03 00:51:27 +0300

    Update Main.scala

--- tasks/term-1/3/src/main/scala/funsets/Main.scala
+++ tasks/term-1/3/src/main/scala/funsets/Main.scala
@@ -2,5 +2,18 @@ package funsets
 
 object Main extends App {
   import FunSets._
-  println(contains(singletonSet(1), 1))
+   println("Single sets")
+  val sngl1 = singletonSet(1)
+  val sngl2 = singletonSet(2) 
+  println("Contains")
+  println(contains(sngl1, 1))
+  println(contains(sngl2, 3))
+  println()
+
+  println("Union")
+  val un1 = union(sngl1, sngl2)
+  printSet(un1)
+  println()
+
+  println("Intersection")
 }
