d71597abf87a55a81ca8e5331afd8c12c3ebc4ba
sergeniys <sergeykartsev2016@gmail.com>
2025-11-10 15:32:01 +0300

    task8
    # Conflicts:
    #	tasks/term-1/6/app/build.gradle

--- tasks/term-1/8/app/build.gradle.kts
+++ tasks/term-1/8/app/build.gradle.kts
@@ -19,6 +19,9 @@ repositories {
 dependencies {
     // This dependency is used by the application.
     implementation(libs.guava)
+    implementation("org.deeplearning4j:deeplearning4j-core:1.0.0-M2.1")
+    implementation("org.nd4j:nd4j-native-platform:1.0.0-M2.1") // CPU backend
+    testImplementation("junit:junit:4.13.2")
 }
 
 testing {
