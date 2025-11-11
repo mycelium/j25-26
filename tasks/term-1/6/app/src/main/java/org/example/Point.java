73ec8d8cef1c81fa9aaf4b5d1da6cd329a1ec83c
vivol <vivoln@mail.ru>
2025-11-11 03:33:16 +0300

    New file - Point.java

---
+++ tasks/term-1/6/app/src/main/java/org/example/Point.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Point {
+    private final double x;
+    private final double y;
+    private final String label;
+
+    public Point(double x, double y, String label) {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double distanceTo(Point other) {
+        double dx = this.x - other.x;
+        double dy = this.y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Point(%.2f, %.2f, %s)", x, y, label);
+    }
+}
