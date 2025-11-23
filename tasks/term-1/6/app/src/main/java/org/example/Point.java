c7e72356988cb71c5c54093167e6b4be42b69f83
Marina <marinavasuk2005@mail.ru>
2025-11-24 00:14:59 +0300

    Update Point.java

---
+++ tasks/term-1/6/app/src/main/java/org/example/Point.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Point {
+    public double x;
+    public double y;
+    public String label;
+
+    public Point(double x, double y, String label) {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+}
