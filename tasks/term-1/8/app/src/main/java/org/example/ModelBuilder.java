2ebbe43500d538f0fa5035250d9a75b0163a8eb3
Shishmakov Vladimir <vova2005761@gmail.com>
2025-12-08 00:56:11 +0300

    task 8 done

---
+++ tasks/term-1/8/app/src/main/java/org/example/ModelBuilder.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+public class ModelBuilder {
+    public MultiLayerNetwork buildModel() {
+        int nChannels = 1; 
+        int outputNum = 10;
+        int seed = 123;
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .updater(new Adam(1e-3))
+                .l2(1e-4)
+                .list()
+                .layer(new ConvolutionLayer.Builder(5, 5)
+                        .nIn(nChannels)
+                        .stride(1, 1)
+                        .nOut(20)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(2, 2)
+                        .stride(2, 2)
+                        .build())
+                .layer(new ConvolutionLayer.Builder(5, 5)
+                        .stride(1, 1)
+                        .nOut(50)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(2, 2)
+                        .stride(2, 2)
+                        .build())
+                .layer(new DenseLayer.Builder()
+                        .nOut(500)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .nOut(outputNum)
+                        .activation(Activation.SOFTMAX)
+                        .build())
+                .setInputType(InputType.convolutionalFlat(28, 28, 1))
+                .build();
+
+        return new MultiLayerNetwork(conf);
+    }
+}
---
+++ tasks/term-1/8/app/src/main/java/org/example/ModelTrainer.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+
+public class ModelTrainer {
+    private final MultiLayerNetwork model;
+    private static final int EPOCHS = 5;
+
+    public ModelTrainer(MultiLayerNetwork model) {
+        this.model = model;
+    }
+
+    public void train(DataSetIterator trainData, DataSetIterator testData) {
+        model.init();
+
+        
+        for (int i = 0; i < EPOCHS; i++) {
+            model.fit(trainData);
+            trainData.reset();
+
+            Evaluation eval = model.evaluate(testData);
+            System.out.println("Epoch " + (i + 1) + " - Accuracy: " + eval.accuracy());
+            testData.reset();
+        }
+    }
+}
