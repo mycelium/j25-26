7838762bcb8c6a4f438d8e38ac2b06da5bb6454a
Egorov SA <egorovsv2005@yandex.ru>
2025-12-14 19:22:21 +0300

    Task 7 is done

--- tasks/term-1/7/MatrixMultPar.java
+++ tasks/term-1/7/MatrixMultPar.java
@@ -1,6 +1,148 @@
-public class MatrixMult {
+public class MatrixMultPar {
 
-	public static double[][] multiplyParallel(double[][] firstMatrix, double[][] secondMatrix){
-		return new double[1][1];
+	private static int NUM_THREADS = Runtime.getRuntime().availableProcessors();
+
+    public static double[][] multiplyParallel(double[][] firstMatrix, double[][] secondMatrix) {
+
+        int firstRowCount = firstMatrix.length;
+        int firstColCount = firstMatrix[0].length;
+        int secondRowCount = secondMatrix.length;
+        int secondColCount = secondMatrix[0].length;
+
+        if (firstColCount != secondRowCount) {
+            throw new IllegalArgumentException("Incorrect sizes for multiply");
+        }
+
+        double[][] resultMatrix = new double[firstRowCount][secondColCount];
+
+        int threadCount = Math.min(NUM_THREADS, firstRowCount);
+        Thread[] threads = new Thread[threadCount];
+
+        int rowsPerThread = firstRowCount / threadCount;
+        int remainingRows = firstRowCount % threadCount;
+
+        int startRowIndex = 0;
+
+        for (int threadIndex = 0; threadIndex < threadCount; threadIndex++) {
+
+            int rowsForCurrentThread =
+                    rowsPerThread + (threadIndex < remainingRows ? 1 : 0);
+
+            int fromRow = startRowIndex;
+            int toRow = fromRow + rowsForCurrentThread;
+
+            threads[threadIndex] = new Thread(() -> {
+
+                for (int i = fromRow; i < toRow; i++) {
+                    for (int k = 0; k < firstColCount; k++) {
+                        double tempValue = firstMatrix[i][k];
+                        for (int j = 0; j < secondColCount; j++) {
+                            resultMatrix[i][j] += tempValue * secondMatrix[k][j];
+                        }
+                    }
+                }
+
+            });
+
+            threads[threadIndex].start();
+            startRowIndex = toRow;
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Thread execution interrupted", e);
+            }
+        }
+
+        return resultMatrix;
+    }
+
+    public static double[][] generateMatrix(int rows, int cols) {
+		int min = 10;
+		int max = 100;
+		double[][] result = new double[rows][cols];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				result[i][j] = (Math.random() * (max - min + 1)) + min;
+			}
+		}
+		return result;
 	}
+
+	public static void printMatrix(double[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.printf("%-10.2f", matrix[i][j]);
+			}
+			System.out.println();
+        }
+	}
+
+    public static int calculateOptimalThreads(int size) {
+        if (size >= 2000) {
+            return NUM_THREADS;
+        }
+
+        int maxTestThreads = 2 * NUM_THREADS;
+        int repeat = 5;
+        double bestTime = Double.MAX_VALUE;
+        int optimalThreads = 1;
+
+        double[][] A = generateMatrix(size, size);
+        double[][] B = generateMatrix(size, size);
+        multiplyParallel(A, B);
+
+        for (int threads = 1; threads <= maxTestThreads; threads++) {
+            NUM_THREADS = threads;
+            long totalTime = 0;
+
+            for (int i = 0; i < repeat; i++) {
+                long startTime = System.currentTimeMillis();
+                multiplyParallel(A, B);
+                long endTime = System.currentTimeMillis();
+                totalTime += (endTime - startTime);
+            }
+
+            double avgTime = (double) totalTime / repeat;
+
+            if (avgTime < bestTime) {
+                bestTime = avgTime;
+                optimalThreads = threads;
+            }
+        }
+
+        return optimalThreads;
+    }
+
+    public static void main(String[] args) {
+        int size = 2000;    //размер матриц
+        int repeat = 10;
+
+        double[][] A = generateMatrix(size, size);
+        double[][] B = generateMatrix(size, size);
+        // printMatrix(A);
+        // printMatrix(B);
+
+        System.out.println("Testing parallel multiply:");
+
+        int threadsToUse = calculateOptimalThreads(size);
+        NUM_THREADS = threadsToUse;
+        System.out.println("Using threads: " + threadsToUse);
+
+        long totalTime = 0;
+
+        for (int i = 0; i < repeat; i++) {
+            long startTime = System.currentTimeMillis();
+            double[][] C = multiplyParallel(A, B);
+            // printMatrix(C);
+            long endTime = System.currentTimeMillis();
+            long duration = endTime - startTime;
+            totalTime += duration;
+        }
+
+        double averageTime = (double) totalTime / repeat;
+        System.out.println("Average time (" + size + " x " + size + "): " + averageTime + " ms\n");
+    }
 }
---
+++ tasks/term-1/7/README.md
@@ -0,0 +1,64 @@
+### 7. Java: Parallel matrix multiplication
+
+- реализовать функцию `public static double[][] multiplyParallel(double[][] firstMatrix, double[][] secondMatrix);`
+- Реализация должна использовать многопоточность
+- Вывести время выполнения для матриц большой размерности
+- Сравнить время с однопоточной реализацией из лабораторной 1
+- Подобрать оптимальное количество потоков под вашу платформу, описать подход к подбору в README
+
+---
+
+#### Подбор оптимального количества потоков
+
+Моя платформа имеет процессор Intel Core i7-1165G7; ядер: 4, логических процессоров (потоков): 8.
+Подбор количества потоков выбирается в зависимости от размера матрицы: если матрица больше размеров 2000x2000, то задается максимальное количество потоков:
+
+
+```java
+private static int NUM_THREADS = Runtime.getRuntime().availableProcessors();
+```
+
+Если же размеры меньше, то вызывается отдельный метод, который "подбирает" подходящее количество потоков методом тестирования. Для этого создаются матрицы случайного размера, а затем происходит по 5 тестовых умножений с разным количеством потоков. То количество, которое показало наименьшее среднее время - используется для дальнейшего умножения.
+
+```java
+public static int calculateOptimalThreads(int size) {
+        if (size >= 2000) { return NUM_THREADS; }
+        int maxTestThreads = 2 * NUM_THREADS;
+        int repeat = 5;
+        double bestTime = Double.MAX_VALUE;
+        int optimalThreads = 1;
+
+        double[][] A = generateMatrix(size, size);
+        double[][] B = generateMatrix(size, size);
+        multiplyParallel(A, B);
+
+        for (int threads = 1; threads <= maxTestThreads; threads++) {
+            NUM_THREADS = threads;
+            long totalTime = 0;
+            for (int i = 0; i < repeat; i++) {
+                long startTime = System.currentTimeMillis();
+                multiplyParallel(A, B);
+                long endTime = System.currentTimeMillis();
+                totalTime += (endTime - startTime);
+            }
+            double avgTime = (double) totalTime / repeat;
+            if (avgTime < bestTime) {
+                bestTime = avgTime;
+                optimalThreads = threads;
+            }
+        }
+        return optimalThreads;
+    }
+```
+То количество, которое показало наименьшее среднее время - используется для дальнейшего умножения. Причем количество потоков может изменяться, так как на малых матрицах затраты на создание и переключение потоков могут быть сопоставимы с самим вычислительным временем.
+
+#### Сравнение времени умножения
+
+Сравнение времен выполнения умножения матриц представлено в таблице (умножение происходит 10 раз на разных матрицах одинаковой размерности, итоговое время - усредняется):
+
+|             |  NonParal  |   Paral   |   Threads   |
+|-------------|------------|-----------|-----------|
+| 100 * 100   | 0.7 ms     | 0.4 ms    | 4 ms      |
+| 500 * 500   | 20.8 ms    | 20.8 ms   | 6.7 ms    |
+| 1000 * 1000 | 165.6 ms   | 165.6 ms  | 76.3 ms   |
+| 2000 * 2000 | 2541.2 ms  | 2541.2 ms | 946.5 ms  |
