90d3d37cc146e944d5bc0079af6f9de10eda0cbc
putyatam <mike.putyata@gmail.com>
2025-12-20 15:02:21 +0300

    fixed implementations in build.gradle.kts

--- tasks/term-1/8/app/build.gradle.kts
+++ tasks/term-1/8/app/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 }
 
 dependencies {
+    implementation(libs.guava)
     implementation("org.deeplearning4j:deeplearning4j-core:1.0.0-M2.1")
     implementation("org.nd4j:nd4j-native-platform:1.0.0-M2.1")
 }
