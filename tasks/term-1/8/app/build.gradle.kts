5abf700474550d04c954a1e7959c540de6a59c62
Aleximiy <166133923+Aleximiy@users.noreply.github.com>
2026-02-17 10:50:03 +0300

    gradle

--- tasks/term-1/8/app/build.gradle.kts
+++ tasks/term-1/8/app/build.gradle.kts
@@ -14,6 +14,9 @@ plugins {
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
+    implementation("org.deeplearning4j:deeplearning4j-core:1.0.0-M2.1")
+    implementation("org.nd4j:nd4j-native-platform:1.0.0-M2.1")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
 }
 
 dependencies {
