5afe33f48632cf48d1a323ba3298f441b3522e78
kozlovskaya-oa <94537045+kozlovskaya-oa@users.noreply.github.com>
2026-02-17 14:36:48 +0300

    Update App.java

--- tasks/term-1/8/app/src/main/java/org/example/App.java
+++ tasks/term-1/8/app/src/main/java/org/example/App.java
@@ -23,7 +23,7 @@ public class App {
         model.evaluate(testIter);
 
         // Путь к изображению"C:\Users\kozlo\Desktop\laaab8\app\src\main\java\resources"
-        String testImagePath = "app/src/main/java/resources/7.png";
+        String testImagePath = "app/src/main/resources/7.png";
         File testImageFile = new File(testImagePath);
 
         System.out.println("Image prediction");
