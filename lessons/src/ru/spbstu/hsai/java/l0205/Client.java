e26b5014658996be988b09c1aa6fd022dfefb5aa
Anton Lukashin <anton.lukashin@crplab.ru>
2026-02-26 17:46:27 +0300

    20260226 classwork

--- lessons/src/ru/spbstu/hsai/java/l0205/Client.java
+++ lessons/src/ru/spbstu/hsai/java/l0205/Client.java
@@ -1,25 +1,36 @@
 package ru.spbstu.hsai.java.l0205;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Client {
-	
+
 	private static final String IP = "127.0.0.1";
 	private static final int PORT = 8080;
-	
+	private static final String image = "image.jpeg";
+
 	public static void main(String[] args) {
 		try (Socket socket = new Socket(IP, PORT)) {
-			Request request = new Request(new byte[10]);
+
+			Path path = Path.of(image);
+			// File file = new File(path);
+			byte[] bytes = Files.readAllBytes(path);
+
+			User user = new User("S", "Sofid", "12345");
+			Request request = new Request(bytes, user);
 			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 			out.writeObject(request);
 			ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-			Response response = (Response)input.readObject();
+			Response response = (Response) input.readObject();
 			System.out.println(response.getTextResponse());
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
 	}
-	
+
 }
--- lessons/src/ru/spbstu/hsai/java/l0205/Request.java
+++ lessons/src/ru/spbstu/hsai/java/l0205/Request.java
@@ -1,22 +1,35 @@
 package ru.spbstu.hsai.java.l0205;
 
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Request implements Serializable {
-	
+
 	private byte[] imageBytes;
+	private User user;
 
 	public Request(byte[] imageBytes) {
 		super();
 		this.imageBytes = imageBytes;
 	}
 
+	public Request(byte[] imageBytes, User user) {
+		super();
+		this.imageBytes = imageBytes;
+		this.user = user;
+	}
+
 	public byte[] getImageBytes() {
 		return imageBytes;
+	}
+	
+	public String getUsers() {
+		return user.toString();
 	}
 
 	public void setImageBytes(byte[] imageBytes) {
 		this.imageBytes = imageBytes;
 	}
-	
+
 }
