6c6814cf3820c3862b874c401b140a05ba72063a
Sofia Meleshchenko <sofia.meleshchenko@gmail.com>
2026-04-09 13:09:54 +0200

    Expand HTTP status codes in Response class

--- tasks/term-2/2/src/httpserver/Response.java
+++ tasks/term-2/2/src/httpserver/Response.java
@@ -50,14 +50,22 @@ public class Response {
 
 	private String getStatusText(int code) {
 		switch (code) {
-		case 200:
-			return "OK";
-		case 404:
-			return "Not Found";
-		case 500:
-			return "Internal Server Error";
-		default:
-			return "Unknown";
-		}
+        case 200: return "OK";
+        case 201: return "Created";
+        case 204: return "No Content";
+        case 301: return "Moved Permanently";
+        case 302: return "Found";
+        case 304: return "Not Modified";
+        case 400: return "Bad Request";
+        case 401: return "Unauthorized";
+        case 403: return "Forbidden";
+        case 404: return "Not Found";
+        case 405: return "Method Not Allowed";
+        case 500: return "Internal Server Error";
+        case 501: return "Not Implemented";
+        case 502: return "Bad Gateway";
+        case 503: return "Service Unavailable";
+        default:  return "Unknown";
+    }
 	}
 }
